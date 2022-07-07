b84dd274e8eac41495a2f2999e3607ced9bd3616
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-07 15:49:24 +0530

    Added 'server.lua' in manifest

--- pvp-siphon/fxmanifest.lua
+++ pvp-siphon/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'Fubuki'
-description 'Adds Health and Armor to Killer '
+description 'Adds Health and Armor to Killer'
 lua54 'yes'
 dependencies 'baseevents' -- https://github.com/citizenfx/cfx-server-data/tree/master/resources/%5Bsystem%5D/baseevents
 
@@ -9,3 +9,5 @@ client_scripts {
     'config.lua',
     'client.lua'
 }
+
+server_script 'server.lua'
