9138d64a7f95ad8887e0b523aab138448d0f8be0
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-07 14:06:34 +0530

    Update fxmanifest.lua

--- pvp-siphon/fxmanifest.lua
+++ pvp-siphon/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 games { 'gta5' }
 author 'Fubuki'
-description 'Adds Health and Armor to Killer'
+description 'Adds Health and Armor to Killer '
 lua54 'yes'
 
 client_scripts {
