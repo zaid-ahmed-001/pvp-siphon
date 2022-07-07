ff9c83e672c38af1212f5ca6e01f14437c974c57
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-07 14:35:21 +0530

    Added dependency for baseevents

--- pvp-siphon/fxmanifest.lua
+++ pvp-siphon/fxmanifest.lua
@@ -3,6 +3,7 @@ games { 'gta5' }
 author 'Fubuki'
 description 'Adds Health and Armor to Killer '
 lua54 'yes'
+dependencies 'baseevents' -- https://github.com/citizenfx/cfx-server-data/tree/master/resources/%5Bsystem%5D/baseevents
 
 client_scripts {
     'config.lua',
