0441b095a9196084984ad57cd55170360dc13432
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-07 14:27:53 +0530

    Added config.lua

---
+++ pvp-siphon/config.lua
@@ -0,0 +1,8 @@
+Config {}
+
+Config.RandomSiphon = false -- Enable this to randomize Health and Armor Amount(100,200) For Heath / (50,100) For Armor
+Config.RandomArmorRange = math.random(50,100) -- Values Can Be Changed here to decide the Random Armor Range (Default Values : 50 to 100 )
+Confog.RandomHealthRange =  math.random(150,200) -- Values Can Be Changed here to decide the Random Health Range (Default Values : 150 to 200 )
+
+Config.HealthAmount = 200 -- Maximum Health Amount(200) 
+Config.ArmorAmount = 100  -- Maximum Armor Amount (100)
