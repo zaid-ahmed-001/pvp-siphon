22e3be9588da4064b53682d4e0d765c43b7f337b
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-07-09 13:55:07 +0530

    Updated client.lua to work with polyzones

--- pvp-siphon/client.lua
+++ pvp-siphon/client.lua
@@ -8,14 +8,35 @@ function siphon()
     else 
         SetPedArmour(playerped, armour + Config.ArmorAmount)
         SetEntityHealth(playerped, health + Config.HealthAmount)			
-    end 													
+    end 							
 end 
 
 AddEventHandler('baseevents:onPlayerKilled', function(killer)
-    TriggerServerEvent('pvp-siphon:server:event', killer)
+    if siphonenabled then 
+        TriggerServerEvent('pvp-siphon:server:event', killer)
+    end 
 end)
 
 AddEventHandler("pvp-siphon:addsiphon")
 RegisterNetEvent("pvp-siphon:addsiphon", function(killer)
 	siphon()	
 end)
+
+local insidesiphonzone1 = false
+Citizen.CreateThread(function()
+    playerped = PlayerPedId()
+    if Config.UsingPolyZones then 
+        while true do
+            coord = GetEntityCoords(playerped)
+            insidesiphonzone1 = Config.siphonzone1:isPointInside(coord)
+            if insidesiphonzone1 then 
+                siphonenabled = true 
+            else 
+                siphonenabled = false 
+            end 
+        Citizen.Wait(500)
+        end
+    else 
+        siphonenabled = true 
+    end 
+end)  
