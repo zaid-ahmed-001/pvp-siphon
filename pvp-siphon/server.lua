4f5fb77589df081911e8163368c35430eb50d94d
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-08-12 20:57:16 +0530

    Update server.lua

--- pvp-siphon/server.lua
+++ pvp-siphon/server.lua
@@ -1,7 +1,13 @@
 RegisterServerEvent('pvp-siphon:server:event')
 AddEventHandler('pvp-siphon:server:event', function(killer, reason)
     local src = source
+    if Config.DevPrints then 
+        print(GetPlayerName(killer), "Killer Name")
+    end 
     if killer then 
         TriggerClientEvent("pvp-siphon:addsiphon", killer, killer) 
+        if Config.DevPrints then 
+            print("Server Event Triggered")
+        end 
     end 
 end)
