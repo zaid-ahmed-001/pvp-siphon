97a0549db82fd8c54ba3312d4ffffe16ad63111a
Fubuki <82799576+Fubukiii01@users.noreply.github.com>
2022-08-12 20:56:49 +0530

    Update client.lua

--- pvp-siphon/client.lua
+++ pvp-siphon/client.lua
@@ -1,31 +1,42 @@
-function siphon()
-    local playerped = PlayerPedId()
-    local armour = GetPedArmour(playerped)
-	local health = GetEntityHealth(playerped)
-    if Config.RandomSiphon then 
-        SetPedArmour(playerped, armour + Config.RandomArmorRange) 
-        SetEntityHealth(playerped, health + Config.RandomHealthRange)
-    else 
-        SetPedArmour(playerped, armour + Config.ArmorAmount)
-        SetEntityHealth(playerped, health + Config.HealthAmount)			
-    end 							
-end 
-
 AddEventHandler('baseevents:onPlayerKilled', function(killer)
+    if Config.DevPrints then 
+        print("Player Killed")
+    end 
     if siphonenabled then 
+        if Config.DevPrints then 
+            print("Client Event Triggered")
+        end 
         TriggerServerEvent('pvp-siphon:server:event', killer)
     end 
 end)
 
 AddEventHandler("pvp-siphon:addsiphon")
 RegisterNetEvent("pvp-siphon:addsiphon", function(killer)
-	siphon()	
+    local playerped = PlayerPedId()
+    local armour = GetPedArmour(playerped)
+	local health = GetEntityHealth(playerped)
+    if Config.RandomSiphon then 
+        SetPedArmour(playerped, armour + Config.RandomArmorRange) 
+        SetEntityHealth(playerped, health + Config.RandomHealthRange)
+        if Config.DevPrints then 
+            print("Siphon Added")
+        end 
+    else 
+        if Config.DevPrints then 
+            print("Siphon Added")
+        end 
+        SetPedArmour(playerped, armour + Config.ArmorAmount)
+        SetEntityHealth(playerped, health + Config.HealthAmount)			
+    end 	
 end)
 
 local insidesiphonzone1 = false
 Citizen.CreateThread(function()
     playerped = PlayerPedId()
     if Config.UsingPolyZones then 
+        if Config.DevPrints then 
+            print("Using PolyZone")
+        end 
         while true do
             coord = GetEntityCoords(playerped)
             insidesiphonzone1 = Config.siphonzone1:isPointInside(coord)
@@ -37,6 +48,9 @@ Citizen.CreateThread(function()
         Citizen.Wait(500)
         end
     else 
+        if Config.DevPrints then 
+            print("Not Using PolyZone")
+        end 
         siphonenabled = true 
     end 
 end)  
